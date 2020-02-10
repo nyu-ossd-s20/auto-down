b3b70f6a2531a27023ad25e428893bc5aec6e1d6
Noah Brumfield <electavire@gmail.com>
2020-02-09 20:42:24 -0500

    Update cage.js

--- ncage-extension/cage.js
+++ ncage-extension/cage.js
@@ -1,5 +1,19 @@
+
+
 Array.prototype.forEach.call(document.querySelectorAll('img'), function (img) {
     var height = img.height;
     var width = img.width;
-    img.src = 'https://www.placecage.com/' + width + '/' + height;
+
+    var rand = (Math.floor(Math.random() * 3))
+    if(rand < 1){
+	     img.src = 'https://www.fillmurray.com/' + width + '/' + height;
+    }
+    else if(rand < 2){
+	     img.src = 'https://www.stevensegallery.com/' + width + '/' + height;
+    }
+    else {
+	     img.src = 'https://www.placecage.com/' + width + '/' + height;
+    }
+    //img.src = 'https://www.placecage.com/' + width + '/' + height;
+
 });
